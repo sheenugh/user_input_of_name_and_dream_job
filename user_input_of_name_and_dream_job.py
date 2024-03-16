caf487b53b63b9b710ba66227fd5daedfd0e2dba
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-03-16 19:37:31 +0800

    Adding Pseudo Code.

---
+++ user_input_of_name_and_dream_job.py
@@ -0,0 +1,17 @@
+# Delima, Sheena Mae D.
+# BSCPE 1-2
+# March 16, 2024
+
+# -----------------------------------------------------------------------------
+
+# # ========= PSEUDO CODE =========
+# || LIBRARIES/PACKAGES ||
+
+
+# || FUNCTIONS ||
+# - Code for the animated text
+# - Code for the content or asking the user about his name and dream job
+
+
+# || ACTUAL CODES ||
+# - 
